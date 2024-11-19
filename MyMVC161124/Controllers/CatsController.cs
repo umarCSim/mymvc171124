b669a3e1a08ea09699cfdb3dbcd420308d5ec5b9
umart1000@gmail.com <umart1000@gmail.com>
2024-11-19 16:47:44 +0000

    Added Cats Controller and some temp data

---
+++ MyMVC161124/Controllers/CatsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMVC161124.Controllers
+{
+    public class CatsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
